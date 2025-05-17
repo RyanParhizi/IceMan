b50d18ead8698ef47e1720d397a8ad1a3497b3d1
Matt7135s <Matt7135s@gmail.com>
2025-05-17 16:30:43 -0700

    Update Actor.h

--- IceMan-skeleton/IceMan/Actor.h
+++ IceMan-skeleton/IceMan/Actor.h
@@ -5,4 +5,54 @@
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
+class Actor : public GraphObject
+{
+public:
+    Actor(int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0)
+    : GraphObject(imageID, startX, startY, dir, size, depth) {
+        setVisible(true);
+    }
+    
+    ~Actor() {}
+
+    virtual void doSomething () = 0;
+    
+    void setAlive(bool alive) {
+        isAlive = alive;
+    }
+
+private:
+    bool isAlive = true;
+};
+
+class Ice : public Actor {
+public:
+    Ice(int startX, int startY)
+    : Actor(IID_ICE, startX, startY, right, 0.25, 3) {}
+    
+    void doSomething() override {}
+};
+
+class Person : public Actor {
+public:
+    Person(int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0)
+    : Actor(imageID, startX, startY, dir, size, depth) {}
+
+    void annoy(int damage) {
+        hitPoints -= damage;
+    }
+
+private:
+    int hitPoints;
+};
+
+class Iceman : public Person {
+public:
+    Iceman()
+    : Person(IID_PLAYER, 30, 60, ...) // TO_DO
+private:
+
+};
+
+
 #endif // ACTOR_H_
