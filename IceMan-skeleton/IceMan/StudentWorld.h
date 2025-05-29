cb859dc96c4199d41518f38227da7fe26d0ebd8c
Ryan Parhizi <ryanparhizi@Ryans-MacBook-Air.local>
2025-05-28 20:11:18 -0700

    ice delete function update (broken)

--- IceMan-skeleton/IceMan/StudentWorld.h
+++ IceMan-skeleton/IceMan/StudentWorld.h
@@ -5,10 +5,7 @@
 #include "GameConstants.h"
 #include <string>
 #include "Actor.h"
-
 #include <array>
-
-// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 
 class StudentWorld : public GameWorld
 {
@@ -24,9 +21,10 @@ public:
     
     virtual int init() {
         player = new Iceman(keyStorage);
-
         iceGridAction(1);
         
+        // iceGrid[player->getX()][player->getY()] = new Ice(player->getX(), player->getY());
+        iceGrid[player->getX()][player->getY()] = nullptr;
         return GWSTATUS_CONTINUE_GAME;
     }
     
@@ -34,26 +32,25 @@ public:
         if(!getKey(keyStorage)){
             keyStorage = 0;
         }
-        player->doSomething();
         
+        player->doSomething();
+        // deleteIceByIndex(player->getX(), player->getY());
+
         return GWSTATUS_CONTINUE_GAME;
     }
     
     virtual void cleanUp() {
         delete player;
         player = nullptr;
-
         iceGridAction(0);
     }
 
 private:
+    
     int keyStorage = 0;
     Iceman* player = nullptr;
-
     std::array<std::array<Ice*, 60>, 64> iceGrid{ nullptr };
-
-	//Vector to other game objects pointers (actor pointers)
-    
+	//Vector to other game objects pointers
 
     void iceGridAction(bool option){ // Option 1 creates ice field. Option 0 destroys ice field.
         int colN = 0;
@@ -73,7 +70,27 @@ private:
             }
             colN++;
         }
-    }
+     }
+
+     bool isIce(int x, int y){
+         if(iceGrid[x][y] == NULL){
+             return 0; // there is no ice at the index
+         }
+         else{
+             return 1; // there is ice at the index
+         }
+     }
+
+     void deleteIceByIndex(int x, int y){
+             for(int i = 0; i < 4; i++){
+                 for(int j = 0; j < 4; j++){
+                    delete iceGrid[x + i][y - j];
+                    iceGrid[x + i][y - j] = nullptr;
+                 }
+             }
+     return;
+     }
+    
 };
 
 #endif // STUDENTWORLD_H_
