0f9119428cffe9562fcad0749a69588d60f6a534
Matt7135s <Matt7135s@gmail.com>
2025-06-05 21:00:02 -0700

    Refactoring
    Following the designs provided on canvas. Old implementations carried over as much as possible.

--- IceMan-skeleton/IceMan/Actor.cpp
+++ IceMan-skeleton/IceMan/Actor.cpp
@@ -1,43 +1,50 @@
 #include "Actor.h"
 #include "StudentWorld.h"
 
-Actor::Actor(int imageID, int startX, int startY, Direction dir, double size, unsigned int depth)
-    : GraphObject(imageID, startX, startY, dir, size, depth) {
-    setVisible(true);
+// Actor __________________________________________________________________________________________
+
+Actor::Actor(StudentWorld* world, int startX, int startY, Direction startDir,
+    bool visible, int imageID, double size, int depth)
+    : GraphObject(imageID, startX, startY, startDir, size, depth),
+    world(world), visible(visible) {
+    setVisible(visible);
 }
 
-Actor::~Actor() {}
 
-void Actor::setAlive(bool alive_) {
-    alive = alive_;
+// Annoy this actor.
+bool Actor::annoy(unsigned int amt) {
+    return false;
+}
+
+// Move this actor to x,y if possible, and return true; otherwise,
+// return false without moving.
+bool Actor::moveToIfPossible(int x, int y) {
+    if (world->canActorMoveTo(this, x, y)) {
+        moveTo(x, y);
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 
-bool Actor::isAlive(){
-    return alive; 
+// Agent __________________________________________________________________________________________
+
+Agent::Agent(StudentWorld* world, int startX, int startY, Direction startDir,
+    int imageID, unsigned int hitPoints)
+    : Actor(world, startX, startY, startDir, true, imageID, 1, 0), hitPoints(hitPoints) {
+
 }
 
-/*----------------------------------Ice---------------------------------------*/
+bool Agent::annoy(unsigned int amount) {
+    return false;
+} //whyIsThisHere;
+bool Agent::canPickThingsUp() const {
+    return false;
+} //whyIsThisHere;
 
-Ice::Ice(int startX, int startY)
-    : Actor(IID_ICE, startX, startY, right, 0.25, 3) {}
-
-Ice::~Ice() {}
-
-void Ice::doSomething() {}
-
-/*----------------------------------Person------------------------------------*/
-
-Person::Person(int imageID, int startX, int startY, Direction dir, double size, unsigned int depth )
-    : Actor(imageID, startX, startY, dir, size, depth) {}
-
-Person::~Person() {}
-
-void Person::annoy(int damage) {
-    hitPoints -= damage;
-}
-
-void Person::processMovementInput(Direction inDir) {
+void Agent::processMovementInput(Direction inDir) {
     if (getDirection() != inDir) {
         setDirection(inDir);
     }
@@ -58,75 +65,322 @@ void Person::processMovementInput(Direction inDir) {
             dy = -1;
             break;
         }
-        moveTo(coordClamp(getX() + dx), coordClamp(getY() + dy));
+        moveToIfPossible(getX() + dx, getY() + dy);
     }
-    return;
 }
 
-int Person::coordClamp(int&& coord) {
-    if (coord < 0) { coord = 0; }
-    if (coord > 60) { coord = 60; }
-    return coord;
+// Iceman _________________________________________________________________________________________
+
+IceMan::IceMan(StudentWorld* world, int startX, int startY)
+    : Agent(world, startX, startY, right, IID_PLAYER, 10) {
+
 }
+void IceMan::move() {
+
+}
+bool IceMan::annoy(unsigned int amount) {
+    return false;
+}
+void IceMan::addGold() {
+
+}
+bool IceMan::canDigThroughIce() const {
+    return false;
+} //whyIsThisHere;
+
+
+// Pick up a sonar kit.
+void IceMan::addSonar() {
+
+}
+
+// Pick up water.
+void IceMan::addWater() {
+
+}
+
+
+// Protester ______________________________________________________________________________________
+
+Protester::Protester(StudentWorld* world, int startX, int startY, int imageID,
+    unsigned int hitPoints, unsigned int score)
+    : Agent(world, startX, startY, left, imageID, hitPoints) {
+
+}
+void Protester::move() {
+
+}
+bool Protester::annoy(unsigned int amount) {
+    return false;
+}
+void Protester::addGold() {
+
+}
+bool Protester::huntsIceMan() const {
+    return false;
+} // whyIsThisHere
+
+
+// RegularProtester _______________________________________________________________________________
+
+RegularProtester::RegularProtester(StudentWorld* world, int startX, int startY, int imageID)
+    : Protester(world, startX, startY, IID_PROTESTER, 5, 100) {
+
+}
+void RegularProtester::move() {
+
+}
+void RegularProtester::addGold() {
+
+}
+
+
+// HardcoreProtester _______________________________________________________________________________
+
+HardcoreProtester::HardcoreProtester(StudentWorld* world, int startX, int startY, int imageID)
+    : Protester(world, startX, startY, IID_HARD_CORE_PROTESTER, 20, 250) {
+
+}
+void HardcoreProtester::move() {
+
+}
+void HardcoreProtester::addGold() {
+
+}
+
+
+// Ice ____________________________________________________________________________________________
+
+Ice::Ice(StudentWorld* world, int startX, int startY)
+    : Actor(world, startX, startY, right, true, IID_ICE, 0.25, 3) { //Curious why GraphObject doesn't work.
+
+}
+void Ice::move() {
+
+}
+
+
+// Boulder ________________________________________________________________________________________
+
+Boulder::Boulder(StudentWorld* world, int startX, int startY)
+    : Actor(world, startX, startY, down, true, IID_BOULDER, 1, 1) {
+
+}
+void Boulder::move() {
+
+}
+bool Boulder::canActorsPassThroughMe() const {
+    return false;
+}//whyIsThisHere
+
+
+// Squirt _________________________________________________________________________________________
+
+Squirt::Squirt(StudentWorld* world, int startX, int startY, Direction startDir)
+    : Actor(world, startX, startY, startDir, true, IID_WATER_SPURT, 1, 1) {
+
+}
+void Squirt::move() {
+
+}
+
+
+// ActivatingObject _______________________________________________________________________________
+
+ActivatingObject::ActivatingObject(StudentWorld* world, int startX, int startY, int imageID,
+    int soundToPlay, bool activateOnPlayer, bool activateOnProtester, bool initallyActive)
+    : Actor(world, startX, startY, right, true, imageID, 1, 2),
+    soundToPlay(soundToPlay), activateOnPlayer(activateOnPlayer),
+    activateOnProtester(activateOnProtester), initiallyActive(initiallyActive){
+
+}
+void ActivatingObject::move() {
+
+}
+
+// Set number of ticks until this object dies
+void ActivatingObject::setTicksToLive() {
+
+}
+
+
+// OilBarrel ______________________________________________________________________________________
+
+OilBarrel::OilBarrel(StudentWorld* world, int startX, int startY)
+    :ActivatingObject(world, startX, startY, IID_BARREL, SOUND_FOUND_OIL,
+        true, false, false) {
+
+}
+void OilBarrel::move() {
+
+}
+bool OilBarrel::needsToBePickedUpToFinishLevel() const {
+    return false;
+} //whyIsThisHere
+
+
+// GoldNugget _____________________________________________________________________________________
+
+GoldNugget::GoldNugget(StudentWorld* world, int startX, int startY, bool temporary)
+    :ActivatingObject(world, startX, startY, IID_GOLD, SOUND_GOT_GOODIE, true,
+    true, true), temporary(temporary) {
+
+}
+void GoldNugget::move() {
+
+}
+
+
+// SonarKit _______________________________________________________________________________________
+
+SonarKit::SonarKit(StudentWorld* world, int startX, int startY)
+    :ActivatingObject(world, startX, startY, IID_SONAR, SOUND_GOT_GOODIE,
+        true, false, true) {
+
+}
+void SonarKit::move() {
+
+}
+
+
+// WaterPool ______________________________________________________________________________________
+
+WaterPool::WaterPool(StudentWorld* world, int startX, int startY)
+    :ActivatingObject(world, startX, startY, IID_WATER_POOL, SOUND_GOT_GOODIE,
+        true, false, true) {
+
+}
+void WaterPool::move() {
+
+}
+
+
+//Actor::Actor(int imageID, int startX, int startY, Direction dir, double size, unsigned int depth)
+//    : GraphObject(imageID, startX, startY, dir, size, depth) {
+//    setVisible(true);
+//}
+//
+//Actor::~Actor() {}
+//
+//void Actor::setAlive(bool alive_) {
+//    alive = alive_;
+//}
+//
+//
+//bool Actor::isAlive(){
+//    return alive; 
+//}
+
+/*----------------------------------Ice---------------------------------------*/
+
+//Ice::Ice(int startX, int startY)
+//    : Actor(IID_ICE, startX, startY, right, 0.25, 3) {}
+//
+//Ice::~Ice() {}
+//
+//void Ice::doSomething() {}
+
+/*----------------------------------Person------------------------------------*/
+
+//Person::Person(int imageID, int startX, int startY, Direction dir, double size, unsigned int depth )
+//    : Actor(imageID, startX, startY, dir, size, depth) {}
+//
+//Person::~Person() {}
+//
+//void Person::annoy(int damage) {
+//    hitPoints -= damage;
+//}
+//
+//void Person::processMovementInput(Direction inDir) {
+//    if (getDirection() != inDir) {
+//        setDirection(inDir);
+//    }
+//    else {
+//        int dx = 0;
+//        int dy = 0;
+//        switch (inDir) {
+//        case right:
+//            dx = 1;
+//            break;
+//        case left:
+//            dx = -1;
+//            break;
+//        case up:
+//            dy = 1;
+//            break;
+//        case down:
+//            dy = -1;
+//            break;
+//        }
+//        moveTo(coordClamp(getX() + dx), coordClamp(getY() + dy));
+//    }
+//    return;
+//}
+//
+//int Person::coordClamp(int&& coord) {
+//    if (coord < 0) { coord = 0; }
+//    if (coord > 60) { coord = 60; }
+//    return coord;
+//}
 
 /*----------------------------------Iceman-------------------------------------*/
 
-Iceman::Iceman(const int& keyRef)
-    : Person(IID_PLAYER, 30, 60, right, 1.0, 0), key(keyRef) {
-
-    hitPoints = 10; 
-    squirtCount = 5;
-    sonarCount = 1;
-    goldCount = 0;
-}
-
-Iceman::~Iceman() {}
-
-void Iceman::doSomething()  {
-    if (!isAlive()) { return; }
-
-    switch(key) {
-    case KEY_PRESS_RIGHT:
-        processMovementInput(right);
-        break;
-    case KEY_PRESS_LEFT:
-        processMovementInput(left);
-        break;
-    case KEY_PRESS_UP:
-        processMovementInput(up);
-        break;
-    case KEY_PRESS_DOWN:
-        processMovementInput(down);
-        break;
-    }
-}
+//Iceman::Iceman(const int& keyRef)
+//    : Person(IID_PLAYER, 30, 60, right, 1.0, 0), key(keyRef) {
+//
+//    hitPoints = 10; 
+//    squirtCount = 5;
+//    sonarCount = 1;
+//    goldCount = 0;
+//}
+//
+//Iceman::~Iceman() {}
+//
+//void Iceman::doSomething()  {
+//    if (!isAlive()) { return; }
+//
+//    switch(key) {
+//    case KEY_PRESS_RIGHT:
+//        processMovementInput(right);
+//        break;
+//    case KEY_PRESS_LEFT:
+//        processMovementInput(left);
+//        break;
+//    case KEY_PRESS_UP:
+//        processMovementInput(up);
+//        break;
+//    case KEY_PRESS_DOWN:
+//        processMovementInput(down);
+//        break;
+//    }
+//}
 
 /*----------------------------------Goodies------------------------------------*/
 
-Goodies::Goodies(int imageID, int startX, int startY, Iceman& playerRef, unsigned int depth)
-    : Actor(imageID, startX, startY, right, 1.0, depth), player(playerRef) {
-    permeance = true;
-}
-
-Goodies::~Goodies() {}
-
-bool Goodies::ispermanent() const {
-    return permeance;
-}
-
-void Goodies::setVisibleToIceman(bool s) {
-    permeance = s;
-    setVisible(s);
-}
+//Goodies::Goodies(int imageID, int startX, int startY, Iceman& playerRef, unsigned int depth)
+//    : Actor(imageID, startX, startY, right, 1.0, depth), player(playerRef) {
+//    permeance = true;
+//}
+//
+//Goodies::~Goodies() {}
+//
+//bool Goodies::ispermanent() const {
+//    return permeance;
+//}
+//
+//void Goodies::setVisibleToIceman(bool s) {
+//    permeance = s;
+//    setVisible(s);
+//}
 
 /*----------------------------------GoldNugget------------------------------------*/
 
 //GoldNugget::GoldNugget(int startX, int startY, bool temporary, int lifetime = 100, bool permeance = true)
 //: Goodies(IID_GOLD, startX, startY, 2) {}
 
-GoldNugget::~GoldNugget() {}
-
-void GoldNugget::doSomething() {}
+//GoldNugget::~GoldNugget() {}
+//
+//void GoldNugget::doSomething() {}
 
 /*
 
--- IceMan-skeleton/IceMan/Actor.h
+++ IceMan-skeleton/IceMan/Actor.h
@@ -3,6 +3,226 @@
 
 #include "GraphObject.h"
 
+class StudentWorld;
+
+class Actor : public GraphObject
+{
+public:
+    Actor(StudentWorld* world, int startX, int startY, Direction startDir,
+        bool visible, int imageID, double size, int depth);
+
+    // Action to perform each tick.
+    virtual void move() = 0;
+
+    // Is this actor alive?
+    bool isAlive() const { return m_isAlive; }
+
+    // Mark this actor as dead.
+    void setDead() { m_isAlive = false; }
+
+    // Annoy this actor.
+    virtual bool annoy(unsigned int amt);
+
+    // Get this actor's world
+    StudentWorld* getWorld() const { return world; }
+
+    // Can other actors pass through this actor?
+    virtual bool canActorsPassThroughMe() const { return m_canActorsPassThroughMe; }
+
+    // Can this actor dig through Ice?
+    virtual bool canDigThroughIce() const { return m_canDigThroughIce; }
+
+    // Can this actor pick items up?
+    virtual bool canPickThingsUp() const { return m_canPickThingsUp; }
+
+    // Does this actor hunt the IceMan?
+    virtual bool huntsIceMan() const { return m_huntsIceMan; }
+
+    // Can this actor need to be picked up to finish the level?
+    virtual bool needsToBePickedUpToFinishLevel() const { return m_needsToBePickedUpToFinishLevel; }
+
+    // Move this actor to x,y if possible, and return true; otherwise,
+    // return false without moving.
+    bool moveToIfPossible(int x, int y);
+
+private:
+    StudentWorld* world;
+    bool visible;
+    bool m_isAlive = true;
+    bool m_canActorsPassThroughMe = true;
+    bool m_canDigThroughIce = false;
+    bool m_canPickThingsUp = false;
+    bool m_huntsIceMan = false;
+    bool m_needsToBePickedUpToFinishLevel = false;
+};
+
+class Agent : public Actor
+{
+public:
+    Agent(StudentWorld* world, int startX, int startY, Direction startDir,
+        int imageID, unsigned int hitPoints);
+
+    // Pick up a gold nugget.
+    virtual void addGold() = 0;
+
+    // How many hit points does this actor have left?
+    unsigned int getHitPoints() const { return hitPoints; }
+
+    virtual bool annoy(unsigned int amount); //whyIsThisHere;
+    virtual bool canPickThingsUp() const; //whyIsThisHere;
+private:
+    int hitPoints; //whatShouldTheDefaultBe
+
+    void Agent::processMovementInput(Direction inDir);
+};
+
+class IceMan : public Agent
+{
+public:
+    IceMan(StudentWorld* world, int startX, int startY);
+    virtual void move();
+    virtual bool annoy(unsigned int amount);
+    virtual void addGold();
+    virtual bool canDigThroughIce() const; //whyIsThisHere;
+
+    // Pick up a sonar kit.
+    void addSonar();
+
+    // Pick up water.
+    void addWater();
+
+    // Get amount of gold
+    unsigned int getGold() const { return gold; }
+
+    // Get amount of sonar charges
+    unsigned int getSonar() const { return sonar; }
+
+    // Get amount of water
+    unsigned int getWater() const { return water; }
+private:
+    int gold; //whatShouldTheDefaultBe
+    int sonar; //whatShouldTheDefaultBe
+    int water; //whatShouldTheDefaultBe
+};
+
+class Protester : public Agent
+{
+public:
+    Protester(StudentWorld* world, int startX, int startY, int imageID,
+        unsigned int hitPoints, unsigned int score);
+    virtual void move();
+    virtual bool annoy(unsigned int amount);
+    virtual void addGold();
+    virtual bool huntsIceMan() const; // whyIsThisHere
+
+    // Set state to having given up protest
+    void setMustLeaveOilField() { mustLeaveOilField = true; }
+
+    // Set number of ticks until next move
+    void setTicksToNextMove() {}
+private:
+    bool mustLeaveOilField;
+    int ticksToNextMove;
+};
+
+class RegularProtester : public Protester
+{
+public:
+    RegularProtester(StudentWorld* world, int startX, int startY, int imageID);
+    virtual void move();
+    virtual void addGold();
+};
+
+class HardcoreProtester : public Protester
+{
+public:
+    HardcoreProtester(StudentWorld* world, int startX, int startY, int imageID);
+    virtual void move();
+    virtual void addGold();
+};
+
+class Ice : public Actor
+{
+public:
+    Ice(StudentWorld* world, int startX, int startY);
+    virtual void move();
+};
+
+class Boulder : public Actor
+{
+public:
+    Boulder(StudentWorld* world, int startX, int startY);
+    virtual void move();
+    virtual bool canActorsPassThroughMe() const; //whyIsThisHere
+};
+
+class Squirt : public Actor
+{
+public:
+    Squirt(StudentWorld* world, int startX, int startY, Direction startDir);
+    virtual void move();
+};
+
+class ActivatingObject : public Actor
+{
+public:
+    ActivatingObject(StudentWorld* world, int startX, int startY, int imageID,
+        int soundToPlay, bool activateOnPlayer,
+        bool activateOnProtester, bool initallyActive);
+    virtual void move();
+
+    // Set number of ticks until this object dies
+    void setTicksToLive();
+private:
+    int soundToPlay;
+    bool activateOnPlayer;
+    bool activateOnProtester;
+    bool initiallyActive;
+    int ticksToLive; //whatShouldTheDefaultBe
+};
+
+class OilBarrel : public ActivatingObject
+{
+public:
+    OilBarrel(StudentWorld* world, int startX, int startY);
+    virtual void move();
+    virtual bool needsToBePickedUpToFinishLevel() const; //whyIsThisHere
+};
+
+class GoldNugget : public ActivatingObject
+{
+public:
+    GoldNugget(StudentWorld* world, int startX, int startY, bool temporary);
+    virtual void move();
+private:
+    bool temporary;
+};
+
+class SonarKit : public ActivatingObject
+{
+public:
+    SonarKit(StudentWorld* world, int startX, int startY);
+    virtual void move();
+};
+
+class WaterPool : public ActivatingObject
+{
+public:
+    WaterPool(StudentWorld* world, int startX, int startY);
+    virtual void move();
+};
+
+#endif // ACTOR_H_
+
+
+
+/*
+
+#ifndef ACTOR_H_
+#define ACTOR_H_
+
+#include "GraphObject.h"
+
 class Actor : public GraphObject
 {
 public:
@@ -90,3 +310,5 @@ class sonarKit : public goodies {
 };
 
 #endif // ACTOR_H_
+
+*/
